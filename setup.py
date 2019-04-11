85769215492028c024a1775f10235cb2cb49df04
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2019-04-11 11:21:04 +0100

    initial fixes to requirements

--- setup.py
+++ setup.py
@@ -14,15 +14,16 @@ setup(
     tests_require=['pytest'],
     install_requires=[
         'numpy>=1.13.3',
-        'torch>=0.4.0',
+        'torch>=1.0.0',
         'scipy>=1.0.0',
         'pandas>=0.21.0',
-        'visdom>=0.1.05',
         'click>=6.7',
-        'xlrd>=1.0.0',
         'cytoolz>=0.9.0.1',
         'tqdm>=4.11.2',
-        'scikit-learn>=0.19.1'
+        'scikit-learn>=0.19.1',
+        'tensorboardX>=1.2',
+        'setuptools>=40.2.0',
+        'torchvision>=0.2.1'
     ],
     packages=['ptavitm']
 )
