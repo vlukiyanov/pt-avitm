11fb34a9728d36be162dab092eb92afa2262fd4d
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2019-04-21 00:11:34 +0100

    Update the sklearn API and documentation (#19)
    * add some very basic loss function tests
    
    * add score function and tests
    
    * some fixes to the coherence calculation
    
    * slightly update the readability of the README example spacing
    
    * attempt to fix travis GCE issue

--- tests/test_sklearn_api.py
+++ tests/test_sklearn_api.py
@@ -1,6 +1,7 @@
 from faker import Faker
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import make_pipeline, Pipeline
 
 from ptavitm.sklearn_api import ProdLDATransformer
 
@@ -21,3 +22,21 @@ def test_basic():
     pipeline.fit([fake.text() for _ in range(100)])
     result = pipeline.transform([fake.text() for _ in range(20)])
     assert result.shape == (20, 50)
+
+
+def test_score():
+    fake = Faker()
+    fake.seed(0)
+    pipeline = Pipeline(steps=[
+        ('vectorizer', CountVectorizer(
+            stop_words='english',
+            max_features=25,
+            max_df=0.9
+        )),
+        ('topic', ProdLDATransformer(epochs=1))
+    ])
+    param_grid = {
+        'topic__epochs': [1, 2, 3],
+    }
+    search = GridSearchCV(pipeline, param_grid, iid=False, cv=2, return_train_score=False)
+    search.fit([fake.text() for _ in range(10)])
