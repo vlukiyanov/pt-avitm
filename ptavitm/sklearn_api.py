some fixes to the coherence calculation

ptavitm/sklearn_api.py
@@ -25,7 +25,7 @@ class ProdLDATransformer(TransformerMixin, BaseEstimator):
                  hidden2_dimension=100,
                  topics=50,
                  lr=0.001,
-                 sample=20000,
+                 samples=20000,
                  score_type='perlexity') -> None:
         self.cuda = torch.cuda.is_available() if cuda is None else cuda
         self.batch_size = batch_size
@@ -34,7 +34,7 @@ class ProdLDATransformer(TransformerMixin, BaseEstimator):
         self.hidden2_dimension = hidden2_dimension
         self.topics = topics
         self.lr = lr
-        self.sample = sample
+        self.samples = samples
         self.autoencoder = None
         self.score_type = score_type
         if self.score_type not in ['perlexity']:
@@ -64,7 +64,7 @@ class ProdLDATransformer(TransformerMixin, BaseEstimator):
             epochs=self.epochs,
             batch_size=self.batch_size,
             optimizer=ae_optimizer,
-            sampler=WeightedRandomSampler(torch.ones(samples), max(samples, self.sample)),
+            sampler=WeightedRandomSampler(torch.ones(samples), max(samples, self.samples)),
             silent=True,
             num_workers=0  # TODO causes a bug to change this on Mac
         )
@@ -89,10 +89,16 @@ class ProdLDATransformer(TransformerMixin, BaseEstimator):
             raise NotFittedError
         self.autoencoder.eval()
         corpus = Sparse2Corpus(X, documents_columns=False)
+        decoder_weight = self.autoencoder.decoder.linear.weight.detach().cpu()
+        id2word = {index: str(index) for index in range(X.shape[1])}
+        topics = [
+            [str(item.item()) for item in topic]
+            for topic in decoder_weight.topk(3, dim=0)[1].t()
+        ]
         cm = CoherenceModel(
-            topics=self.topics,
+            topics=topics,
             corpus=corpus,
-            dictionary=Dictionary.from_corpus(corpus),
+            dictionary=Dictionary.from_corpus(corpus, id2word),
             coherence='u_mass'
         )
         return cm.get_coherence()